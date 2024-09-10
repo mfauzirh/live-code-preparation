1fd127a1dc761bb29849196526c269a3283bdd08
mfauzirh <mfauzirizkih@gmail.com>
2024-09-10 16:05:07 +0700

    initialize farm app

---
+++ farm-app/src/test/java/com/mfauzirh/farmapp/FarmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfauzirh.farmapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
