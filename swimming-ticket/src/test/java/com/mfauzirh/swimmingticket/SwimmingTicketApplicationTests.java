c7c32fa5ff72eeb531ea5dc8f9395fd4d7de7185
mfauzirh <mfauzirizkih@gmail.com>
2024-09-10 14:05:26 +0700

    initialize swimming ticket project

---
+++ swimming-ticket/src/test/java/com/mfauzirh/swimmingticket/SwimmingTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfauzirh.swimmingticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwimmingTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
